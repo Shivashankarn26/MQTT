5cceeb1e86d63d055dc39aa9d8679643504d7bb2
Shivashankar Nataraju <shivashankar.nataraju@autostoresystem.com>
2022-11-23 23:02:51 +0100

    Create MQTT subscriber
    New project Subscriber is added to the solution to perform
    the subscription for the MQTT message

---
+++ Subscriber/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
+
+var factory = new MqttFactory();
+var client = factory.CreateMqttClient();
+var options = new MqttClientOptionsBuilder()
+    .WithClientId(Guid.NewGuid().ToString())
+    .WithTcpServer("test.mosquitto.org", 1883)
+    .WithCleanSession()
+    .Build();
+
+client.ConnectedAsync += Client_Connected;
+
+async Task Client_Connected(MqttClientConnectedEventArgs arg)
+{
+    Console.WriteLine("Connected..!");
+    var topic = new MqttTopicFilterBuilder()
+        .WithTopic("SampleMessage")
+        .Build();
+    await client.SubscribeAsync(topic);
+    //return;
+}
+
+client.DisconnectedAsync += Client_Disconnected;
+async Task Client_Disconnected(MqttClientDisconnectedEventArgs arg)
+{
+    Console.WriteLine("Disconnected..!");
+    //return null;
+}
+
+client.ApplicationMessageReceivedAsync += Client_MessageReceived;
+async Task Client_MessageReceived(MqttApplicationMessageReceivedEventArgs arg)
+{
+    Console.WriteLine("Message Received: " + Encoding.UTF8.GetString(arg.ApplicationMessage.Payload));
+}
+
+await client.ConnectAsync(options);
+
+Console.ReadLine();
+
+await client.DisconnectAsync();
